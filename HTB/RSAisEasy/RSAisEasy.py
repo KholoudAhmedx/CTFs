b850be4249c8cebce97b905111382887dd9e1483
kholoudahmedx <kholoudahmedmuhamed4@gmail.com>
2024-12-21 21:27:52 +0200

    RSAisEasy

---
+++ HTB/RSAisEasy/RSAisEasy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from Crypto.Util.number import bytes_to_long, getPrime
+from secrets import flag1, flag2
+from os import urandom
+
+flag1 = bytes_to_long(flag1)
+flag2 = bytes_to_long(flag2)
+
+p, q, z = [getPrime(512) for i in range(3)]
+
+print(f'p: {p}')
+print(f'q:{q}')
+print(f'z:{z}')
+
+e = 0x10001
+
+n1 = p * q
+n2 = q * z
+
+c1 = pow(flag1, e, n1)
+c2 = pow(flag2, e, n2)
+
+E = bytes_to_long(urandom(69))
+
+print(f'n1: {n1}')
+print(f'c1: {c1}')
+print(f'c2: {c2}')
+print(f'(n1 * E) + n2: {n1 * E + n2}')
---
+++ HTB/RSAisEasy/crackRSA.py
@@ -0,0 +1,82 @@
+from math import gcd
+from Crypto.Util.number import long2str, long_to_bytes, bytes_to_long, getRandomNBitInteger
+from sympy import isprime
+
+n1= 101302608234750530215072272904674037076286246679691423280860345380727387460347553585319149306846617895151397345134725469568034944362725840889803514170441153452816738520513986621545456486260186057658467757935510362350710672577390455772286945685838373154626020209228183673388592030449624410459900543470481715269
+c1= 92506893588979548794790672542461288412902813248116064711808481112865246689691740816363092933206841082369015763989265012104504500670878633324061404374817814507356553697459987468562146726510492528932139036063681327547916073034377647100888763559498314765496171327071015998871821569774481702484239056959316014064
+c2= 46096854429474193473315622000700040188659289972305530955007054362815555622172000229584906225161285873027049199121215251038480738839915061587734141659589689176363962259066462128434796823277974789556411556028716349578708536050061871052948425521408788256153194537438422533790942307426802114531079426322801866673
+e = 0x10001
+e_decimal = int(e)
+#print(f'{e_decimal}')
+
+# This equation -> (n1 * E) + n2
+# Solve it in terms of n2 -> /// n2 = value_of_equation - (n1 * E)
+value_of_equation = 601613204734044874510382122719388369424704454445440856955212747733856646787417730534645761871794607755794569926160226856377491672497901427125762773794612714954548970049734347216746397532291215057264241745928752782099454036635249993278807842576939476615587990343335792606509594080976599605315657632227121700808996847129758656266941422227113386647519604149159248887809688029519252391934671647670787874483702292498358573950359909165677642135389614863992438265717898239252246163
+
+# Solution?
+# Vulnerability: Duplicate prime used in n1 and n2 which in our case :q
+# gcd(n1,n2) = q, so we can compute gcd , to know what the value of q is
+# get the value of p (the second prime) = n1(given) // q, and you know get the both value of primes
+# You can easily crack the private key and get the flag
+
+
+# Bruteforce value of E (failed solution)
+# for E in range(1,100000):
+#     n2 = value_of_equation- (n1 * E)
+
+#     q = gcd(n1, n2)
+
+#     # How to make sure q is the correct value? if it is greater than 1
+#     if q > 1 & isprime(q) : 
+#         #print(f'We found the value of q :{q}')
+#         break
+
+#     # print(f'Value of n2 :{n2}')
+#     # Now that we have both n1 and q -> get p
+
+## To get n2 -> use modulu properties  (Successed solution)
+### Take modoulu n1 of the two parts of the equation to get rid of multiples of n1 which is (n1 * E) part in the equation -: 
+### ((n1 * E) + n2 ) % n1 = value_of_equation % n1
+## By simplifying the terms: 
+### (n1 * E) % n1 + (n2 % n1) = value_of_equation % n1
+###  0 + n2 % n1 = value_of_equation % n1
+###  n2 %n1 = value_of_equation % n1
+###  This simplifies to :=> n2 = value_of_equation % n1 (which means you can calculate value of n2 without bruteforcing value of E)
+
+n2 = value_of_equation % n1
+q = gcd(n1, n2)
+
+p = n1 // q
+z = n2 // q 
+# print(f'Value of p :{p}')
+# print(f'Value of z :{z}')
+
+
+# Compute totient function phi_n1 , phi_n2
+phi_n1 = (p - 1)*(q - 1)
+phi_n2 = (z - 1)*(q - 1)
+
+
+# print(f'Value of phi_n1: {phi_n1}')
+# print(f'Value of phi_n2: {phi_n2}')
+
+# Compute private key d1 , d2
+d1 = pow(e_decimal,-1,phi_n1)
+d2 = pow(e_decimal, -1, phi_n2)
+
+# print(f'Value of d1: {d1}')
+# print(f'Value of d2: {d2}')
+
+# Decrypt c1 and c2 
+m1 = pow(c1, d1, n1)
+m2 = pow(c2, d2, n2)
+
+
+# print(f'Value of m1: {m1}')
+# print(f'Value of m2: {m2}')
+
+flag1 = long_to_bytes(m1)
+flag2 = long_to_bytes(m2)
+
+print(f'Value of flag1: {flag1}') ## Here's the flag 
+print(f'Value of flag2 : {flag2}')
